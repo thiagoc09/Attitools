8642db958457643a925c328148f054629d5e8a96
PedroZotelli <pzotelli2@gmail.com>
2021-05-27 10:29:58 -0300

    rota para empresas

--- routes/index.ts
+++ routes/index.ts
@@ -8,6 +8,10 @@ class Index {
 	public async cadastro(req: app.Request, res: app.Response) {
 		res.sendFile(app.dir.views + "/cadastro.html");
 	}
+
+	public async empresas(req: app.Request, res: app.Response) {
+		res.sendFile(app.dir.views + "/empresas.html");
+	}
 }
 
 export = Index;
